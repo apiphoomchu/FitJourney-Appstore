ff8aa5ae49d9fab17e33eb1d52b4aec58697366d
Api Phoom <jj3622th@gmail.com>
2024-03-27 14:00:10 +0700

    Initial Commit

---
+++ FitJourney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitJourney
+//
+//  Created by Apiphoom Chuenchompoo on 27/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitJourney/FitJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitJourneyApp.swift
+//  FitJourney
+//
+//  Created by Apiphoom Chuenchompoo on 27/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct FitJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
